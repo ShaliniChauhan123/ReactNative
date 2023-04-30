4966ae5bede65a577a12e5d596db4d48aa697967
Shalini <chauhanshaliniuk@gmail.com>
2023-04-30 14:38:14 +0530

    commit

--- App.tsx
+++ App.tsx
@@ -5,27 +5,23 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
-  Button,
-  SafeAreaView,
+  Image,
+  Platform,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   TextInput,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import {useMutation} from 'react-query';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -64,45 +60,81 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const clickHandler = () => {
-    console.log('first');
-  };
+  const {mutate} = useMutation(
+    ['addTodo'],
+    async (options: {description: string}) => {
+      console.log(
+        'addTodo',
+        JSON.stringify({
+          title: 'newtitle',
+          technologies: 'technologies1',
+          description: options.description,
+          budget: 90.9,
+          contact_email: 'shalinic@gluelabs.com',
+        }),
+      );
+      const res = await fetch('http://localhost:3000/api/tech', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: 'newtitle',
+          technologies: 'technologies1',
+          description: options.description,
+          budget: 90.9,
+          contact_email: 'shalinic@gluelabs.com',
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+          Authorization:
+            'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJtdWgubnVyYWxpNDNAZ21haWwuY29tIiwicGFzc3dvcmQiOiIkMmEkMTAkMkxIaEE2RXVuU21ockgwV3hZclFOLm13dVRHbWMzMjZ4aW0zR0xjdlUyazUzcXZHWEY5Z1ciLCJjcmVhdGVkQXQiOiIyMDIzLTA0LTI4VDEwOjMwOjAyLjg1N1oiLCJ1cGRhdGVkQXQiOiIyMDIzLTA0LTI4VDEwOjMwOjAyLjg1N1oiLCJpYXQiOjE2ODI2NzkzNTcsImV4cCI6MTY4NTI3MTM1N30.dyhVSu6OHxENID0e0a8xQwaTeCz8OUXFIvdiXJu8hKw',
+        },
+      });
+      console.log('### res', res);
+      return res;
+    },
+  );
 
+  const [inputVal, setInputVal] = useState('');
   return (
-    <SafeAreaView style={backgroundStyle}>
-      {/* <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      /> */}
+    <View style={styles.MainContainer}>
       <Section title="My Todo App" />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <View
-            style={[
-              {
-                flexDirection: 'row',
-                width: '100%',
-              },
-            ]}>
-            <TextInput
-              style={{
-                height: 40,
-                borderColor: 'gray',
-                borderWidth: 1,
-                width: '50%',
-              }}>
-              Type your To do...
-            </TextInput>
-            <Button title="button" onPress={clickHandler} />
-          </View>
-        </View>
+      <TextInput
+        value={inputVal}
+        onChangeText={newText => setInputVal(newText)}
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+          width: '100%',
+          borderRadius: 50,
+          padding: 20,
+        }}
+        placeholder="Add your Todo..."></TextInput>
+
+      <View style={styles.bottomView}>
+        <TouchableOpacity
+          onPress={() => {
+            console.log('first', inputVal);
+            mutate({
+              description: inputVal,
+            });
+          }}
+          style={styles.touchableOpacityStyle}>
+          <Image
+            source={{
+              uri: 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/plus_icon.png',
+            }}
+            style={styles.floatingButtonStyle}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView>
+        <Text>hhh</Text>
+        <Text>hhh</Text>
+        <Text>hhh</Text>
+        <Text>hhh</Text>
       </ScrollView>
-    </SafeAreaView>
+    </View>
   );
 }
 
@@ -122,6 +154,38 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+
+  touchableOpacityStyle: {
+    position: 'absolute',
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 30,
+    bottom: 30,
+  },
+  floatingButtonStyle: {
+    resizeMode: 'contain',
+    width: 50,
+    height: 50,
+  },
+  MainContainer: {
+    flex: 1,
+    paddingTop: Platform.OS === 'ios' ? 40 : 0,
+    padding: 20,
+  },
+
+  bottomView: {
+    width: '100%',
+    height: 50,
+    position: 'absolute',
+    bottom: 0,
+  },
+
+  textStyle: {
+    color: '#fff',
+    fontSize: 22,
   },
 });
 
